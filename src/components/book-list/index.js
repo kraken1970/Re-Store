968e19569035bdc135ab5004a9ec1eaa35e462de
Serg Minin <everestdon@ukr.net>
2019-04-27 20:22:35 +0300

    added thunk

--- src/components/book-list/index.js
+++ src/components/book-list/index.js
@@ -1,55 +1,96 @@
 import React, { Component } from "react";
 import BookListItem from "../book-list-item";
 import { connect } from "react-redux";
-
+import { bindActionCreators } from "redux";
 import { withBookstoreService } from "../hoc";
-import { booksLoaded } from "../../actions";
+import { fetchBooks, bookAddedToCart } from "../../actions";
+import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
 // import { compose } from "../../utils";
 import "./book-list.css";
 
-class BookList extends Component {
-  componentDidMount() {
-    //1. receive data
-    const { bookstoreService } = this.props;
-    const data = bookstoreService.getBooks();
+const BookList = ({ books, onAddedToCart }) => {
+  return (
+    <ul className="book-list">
+      {books.map(book => {
+        return (
+          <li key={book.id}>
+            <BookListItem
+              book={book}
+              onAddedToCart={() => onAddedToCart(book.id)}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-    //2. dispatch action to store
-    this.props.booksLoaded(data);
+class BookListContainer extends Component {
+  componentDidMount() {
+    // const {
+    //   bookstoreService,
+    //   booksLoaded,
+    //   booksRequested,
+    //   booksError
+    // } = this.props;
+    // booksRequested();
+    // bookstoreService
+    //   .getBooks()
+    //   .then(data => {
+    //     booksLoaded(data);
+    //   })
+    //   .catch(err => booksError(err));
+    this.props.fetchBooks();
   }
 
   render() {
-    const { books } = this.props;
-    return (
-      <ul>
-        {books.map(book => {
-          return (
-            <li key={book.id}>
-              <BookListItem book={book} />
-            </li>
-          );
-        })}
-      </ul>
-    );
+    const { books, loading, error, onAddedToCart } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    }
+
+    if (error) {
+      return <ErrorIndicator />;
+    }
+
+    return <BookList books={books} onAddedToCart={onAddedToCart} />;
   }
 }
 
-const mapStateToProps = ({ books }) => {
+const mapStateToProps = ({ bookList: { books, loading, error } }) => {
   return {
-    books
+    books,
+    loading,
+    error
   };
 };
 
-const mapDispatchToProps = {
-  booksLoaded
+// const mapDispatchToProps = {
+//   booksLoaded,
+//   booksRequested,
+//   booksError
+// };
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const { bookstoreService } = ownProps;
+  return bindActionCreators(
+    {
+      fetchBooks: fetchBooks(bookstoreService),
+      onAddedToCart: bookAddedToCart
+    },
+    dispatch
+  );
 };
 
 export default withBookstoreService()(
   connect(
     mapStateToProps,
     mapDispatchToProps
-  )(BookList)
+  )(BookListContainer)
 );
-// version with compose:
+// //version with compose:
 // export default compose(
 //   withBookstoreService(),
 //   connect(
--- src/store.js
+++ src/store.js
@@ -1,7 +1,39 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
 
 import reducer from "./reducers";
+// next - по сути - следующий dispatch
+const logMiddleware = ({ getState }) => next => action => {
+  console.log(action.type, getState());
+  return next(action);
+};
 
-const store = createStore(reducer);
+const stringMiddleware = () => next => action => {
+  if (typeof action === "string") {
+    return next({
+      type: action
+    });
+  }
+  return next(action);
+};
+
+//!ВАЖНО соблюдать порядок:
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware, stringMiddleware, logMiddleware)
+);
+
+const delayedActionCreator = timeout => dispatch => {
+  setTimeout(
+    () =>
+      dispatch({
+        type: "DELAYED_ACTION"
+      }),
+    timeout
+  );
+};
+
+store.dispatch(delayedActionCreator(3000));
 
 export default store;
